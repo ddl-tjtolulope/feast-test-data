294d234f85b39d66a06640a1139c1818fc816cd8
ddl-joyce-zhao <joyce.zhao@dominodatalab.com>
2023-02-22 15:46:50 -0500

    updated features

--- feast-redshift/feature_driver_stats.py
+++ feast-redshift/feature_driver_stats.py
@@ -1,7 +1,8 @@
+# This is an example feature definition file
+
 from datetime import timedelta
 
 import pandas as pd
-import yaml
 
 from feast import (
     Entity,
@@ -9,57 +10,49 @@ from feast import (
     FeatureView,
     Field,
     PushSource,
+    RedshiftSource,
     RequestSource,
-    SnowflakeSource,
 )
 from feast.on_demand_feature_view import on_demand_feature_view
 from feast.types import Float32, Float64, Int64
 
 # Define an entity for the driver. You can think of an entity as a primary key used to
 # fetch features.
-driver = Entity(name="driver", join_keys=["DRIVER_ID"])
+driver = Entity(name="driver", join_keys=["driver_id"])
 
 # Defines a data source from which feature values can be retrieved. Sources are queried when building training
 # datasets or materializing features into an online store.
-project_name = yaml.safe_load(open("feature_store.yaml"))["project"]
-
-driver_stats_source = SnowflakeSource(
-    # The Snowflake table where features can be found
-    database=yaml.safe_load(open("feature_store.yaml"))["offline_store"]["database"],
-    table="DRIVER_STATS",
+driver_stats_source = RedshiftSource(
+    # The Redshift table where features can be found
+    table="driver_stats",
     # The event timestamp is used for point-in-time joins and for ensuring only
     # features within the TTL are returned
-    timestamp_field="EVENT_TIMESTAMP",
+    timestamp_field="event_timestamp",
     # The (optional) created timestamp is used to ensure there are no duplicate
     # feature rows in the offline store or when building training datasets
-    created_timestamp_column="CREATED",
+    created_timestamp_column="created",
+    # Database to redshift source.
+    database="dev",
 )
 
-# Feature views are a grouping based on how features are stored in either the
-# online or offline store.
+# Our parquet files contain sample data that includes a driver_id column, timestamps and
+# three feature column. Here we define a Feature View that will allow us to serve this
+# data to our model online.
 driver_stats_fv = FeatureView(
     # The unique name of this feature view. Two feature views in a single
     # project cannot have the same name
     name="driver_hourly_stats",
-    # The list of entities specifies the keys required for joining or looking
-    # up features from this feature view. The reference provided in this field
-    # correspond to the name of a defined entity (or entities)
     entities=[driver],
-    # The timedelta is the maximum age that each feature value may have
-    # relative to its lookup time. For historical features (used in training),
-    # TTL is relative to each timestamp provided in the entity dataframe.
-    # TTL also allows for eviction of keys from online stores and limits the
-    # amount of historical scanning required for historical feature values
-    # during retrieval
-    ttl=timedelta(weeks=52 * 10),  # Set to be very long for example purposes only
+    ttl=timedelta(days=1),
     # The list of features defined below act as a schema to both define features
     # for both materialization of features into a store, and are used as references
     # during retrieval for building a training dataset or serving features
     schema=[
-        Field(name="CONV_RATE", dtype=Float32),
-        Field(name="ACC_RATE", dtype=Float32),
-        Field(name="AVG_DAILY_TRIPS", dtype=Int64),
+        Field(name="conv_rate", dtype=Float32),
+        Field(name="acc_rate", dtype=Float32),
+        Field(name="avg_daily_trips", dtype=Int64),
     ],
+    online=True,
     source=driver_stats_source,
     # Tags are user defined key/value pairs that are attached to each
     # feature view
@@ -88,8 +81,8 @@ input_request = RequestSource(
 )
 def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
     df = pd.DataFrame()
-    df["conv_rate_plus_val1"] = inputs["CONV_RATE"] + inputs["val_to_add"]
-    df["conv_rate_plus_val2"] = inputs["CONV_RATE"] + inputs["val_to_add_2"]
+    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
+    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
     return df
 
 
@@ -97,7 +90,7 @@ def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
 driver_activity_v1 = FeatureService(
     name="driver_activity_v1",
     features=[
-        driver_stats_fv[["CONV_RATE"]],  # Sub-selects a feature from a feature view
+        driver_stats_fv[["conv_rate"]],  # Sub-selects a feature from a feature view
         transformed_conv_rate,  # Selects all features from the feature view
     ],
 )
@@ -117,11 +110,11 @@ driver_stats_push_source = PushSource(
 driver_stats_fresh_fv = FeatureView(
     name="driver_hourly_stats_fresh",
     entities=[driver],
-    ttl=timedelta(weeks=52 * 10),  # Set to be very long for example purposes only
+    ttl=timedelta(days=1),
     schema=[
-        Field(name="CONV_RATE", dtype=Float32),
-        Field(name="ACC_RATE", dtype=Float32),
-        Field(name="AVG_DAILY_TRIPS", dtype=Int64),
+        Field(name="conv_rate", dtype=Float32),
+        Field(name="acc_rate", dtype=Float32),
+        Field(name="avg_daily_trips", dtype=Int64),
     ],
     online=True,
     source=driver_stats_push_source,  # Changed from above
@@ -140,8 +133,8 @@ driver_stats_fresh_fv = FeatureView(
 )
 def transformed_conv_rate_fresh(inputs: pd.DataFrame) -> pd.DataFrame:
     df = pd.DataFrame()
-    df["conv_rate_plus_val1"] = inputs["CONV_RATE"] + inputs["val_to_add"]
-    df["conv_rate_plus_val2"] = inputs["CONV_RATE"] + inputs["val_to_add_2"]
+    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
+    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
     return df
 
 
